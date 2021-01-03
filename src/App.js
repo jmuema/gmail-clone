f48f70191e7e04bd3dd3e3eb1fe3486c92060110
jmuema <jmuema95@gmail.com>
2021-01-03 16:34:29 +0300

    added react router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import EmailList from './EmailList';
+import Mail from './Mail';
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <Sidebar/>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/mail">
+
+              <Mail />
+            </Route>
+            <Route path="/">
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
       </div>
+
+    </Router>
+
 
   );
 }
