4ab050312c5f7fe562cd5a240b77e043109b5dc2
jmuema <jmuema95@gmail.com>
2021-01-03 16:55:22 +0300

    working on the emaillist component

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './EmailList.css';
+import { IconButton, Checkbox } from "@material-ui/core";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+
 
 function EmailList() {
     return (
-        <div className="EmailList">
-            
+        <div className="emailList">
+            <div className="emailList__settings">
+                <div className="emailList__settingsLeft">
+                    <Checkbox/>
+                    <IconButton>
+                       <ArrowDropDownIcon/> 
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon/>
+                        </IconButton>
+                        <IconButton>
+                        <MoreVertIcon/>
+                        </IconButton>
+
+                </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                     <ChevronLeftIcon/>   
+                    </IconButton>
+                    <IconButton>
+                    <ChevronRightIcon/>   
+                        </IconButton>
+                        <IconButton>
+                        <KeyboardHideIcon/>
+                        </IconButton>
+                        <IconButton>
+                        <SettingsIcon/>
+                        </IconButton>
+                </div>
+            </div>
+            <div className="emailList__sections">
+
+            </div>
             
         </div>
     )
