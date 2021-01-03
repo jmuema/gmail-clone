d36c48be141b7ce29df49d72e579d617631fe7c0
jmuema <jmuema95@gmail.com>
2021-01-03 17:11:24 +0300

    created the sections top part/icons

--- src/Section.js
+++ src/Section.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Section.css';
 
-function Section() {
+function Section({ Icon, title, color, selected}) {
     return (
-        <div className="section">
-            
-        </div>
+        <div className={`section ${selected && 'section--selected'}`}
+        style={{ 
+            borderBottom: `3px solid ${color}`,
+            color:`${selected && color }`,
+        }}
+         >
+             <Icon/>
+             <h4>{title}</h4>
+         </div>
     )
 }
 
